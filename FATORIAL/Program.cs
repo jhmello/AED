6f74bb637722a5b9a43bdf9003fcda45169a6664
720508 <720508@PSG10INFLB20511>
2018-02-03 10:29:39 -0200

    Add project files.

---
+++ FATORIAL/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FATORIAL
+{
+    class Program
+    {
+        static public int _Fatorial(int N)
+        {
+            if (N == 1)
+            {
+                return 1;
+            }
+            return N * _Fatorial(N - 1);
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Fatorial de 5: " + _Fatorial(5));
+            Console.ReadKey();
+        }
+    }
+}
